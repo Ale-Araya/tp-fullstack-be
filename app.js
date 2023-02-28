98c594da1274cf686f4c00ad352cfcfdb82bbf2b
Alejandro <ale_aleara@hotmail.com>
2023-02-28 12:15:05 -0300

    body parser

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
 const usersRouter= require("./routes/users");
 
 const dbConnect=require("./database/dbConnection")
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 app.use("/users",usersRouter);
 
 app.listen(port, () => {
